f16a096499a6b0c5a4c6dcc7ee7f4e66ef7f9db5
HashtagMarky <samuel.mark01@outlook.com>
2023-09-30 21:25:42 +0100

    Update Snorlax sprite position

--- src/main_menu.c
+++ src/main_menu.c
@@ -1628,7 +1628,7 @@ static void Task_NewGameBirchSpeech_ReshowBirchLotad(u8 taskId)
         gSprites[spriteId].oam.objMode = ST_OAM_OBJ_BLEND;
         spriteId = gTasks[taskId].tLotadSpriteId;
         gSprites[spriteId].x = 100;
-        gSprites[spriteId].y = 75;
+        gSprites[spriteId].y = 65;
         gSprites[spriteId].invisible = FALSE;
         gSprites[spriteId].oam.objMode = ST_OAM_OBJ_BLEND;
         NewGameBirchSpeech_StartFadeInTarget1OutTarget2(taskId, 2);
