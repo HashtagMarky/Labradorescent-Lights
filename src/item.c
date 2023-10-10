1bb60401ae7cf9842e38a66b95c5651b06626d07
HashtagMarky <samuel.mark01@outlook.com>
2023-10-10 15:03:46 +0100

    Really shows item off of screen (Item Desc Header)

--- src/item.c
+++ src/item.c
@@ -1108,6 +1108,8 @@ static void ShowItemIconSprite(u16 item, bool8 firstTime, bool8 flash)
             // This shows the item in the message box
             //x = 213;
             //y = 140;
+            x = -50;
+            y = -50;
         }
         else
         {
