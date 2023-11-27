6028b64decd8d4d27cca6a3ae072de266562d7c1
HashtagMarky <samuel.mark01@outlook.com>
2023-11-27 19:29:41 +0000

    Catch Tutorial is Swablu not Zigzagoon

--- src/field_specials.c
+++ src/field_specials.c
@@ -1392,13 +1392,14 @@ void SetShoalItemFlag(u16 unused)
 void PutZigzagoonInPlayerParty(void)
 {
     u16 monData;
-    CreateMon(&gPlayerParty[0], SPECIES_ZIGZAGOON, 7, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
+    CreateMon(&gPlayerParty[0], SPECIES_SWABLU, 7, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
     monData = TRUE;
     SetMonData(&gPlayerParty[0], MON_DATA_ABILITY_NUM, &monData);
-    monData = MOVE_TACKLE;
+    monData = MOVE_PECK;
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE1, &monData);
-    monData = MOVE_NONE;
+    monData = MOVE_GROWL;
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE2, &monData);
+    monData = MOVE_NONE;
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE3, &monData);
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE4, &monData);
 }
