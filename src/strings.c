46ce7d3bc6a1da97ef66f24cfc7a99ef4619a90e
HashtagMarky <samuel.mark01@outlook.com>
2024-03-04 15:17:23 +0000

    Fix Match Call Tutorial Start Menu

--- src/strings.c
+++ src/strings.c
@@ -1785,6 +1785,7 @@ const u8 gText_PokeBalls[] = _("POKé BALLS");
 const u8 gText_Berry[] = _("BERRY");
 const u8 gText_Berries[] = _("BERRIES");
 const u8 gText_Nickname[] = _("NICKNAME");
+const u8 gText_Quests[] = _("QUESTS");
 
 //// side quest data
 //generic strings
