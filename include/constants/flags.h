cf7c5c601e2cf417f0f2a5bdc42c26b964065ed9
HashtagMarky <samuel.mark01@outlook.com>
2024-04-18 17:01:00 +0100

    Add Homely Family on Route 103 Mountains

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -74,7 +74,7 @@
 #define FLAG_ESPURR_GENDER              0x3C // Unused Flag
 #define FLAG_HIDE_OLDALE_FAMILY         0x3D // Unused Flag
 #define FLAG_HIDE_ROUTE103MT_FAMILY     0x3E // Unused Flag
-#define FLAG_UNUSED_0x03F    0x3F // Unused Flag
+#define FLAG_HIDE_QUEEN_ABSOL           0x3F // Unused Flag
 #define FLAG_UNUSED_0x040    0x40 // Unused Flag
 #define FLAG_UNUSED_0x041    0x41 // Unused Flag
 #define FLAG_UNUSED_0x042    0x42 // Unused Flag
