2cb43bedf76f3e231243f56e05764184e399a8ae
HashtagMarky <samuel.mark01@outlook.com>
2024-04-23 16:42:24 +0100

    Update Whiteout Flag Clears

--- src/overworld.c
+++ src/overworld.c
@@ -420,7 +420,9 @@ static void Overworld_ResetStateAfterWhiteOut(void)
     FlagClear(FLAG_SYS_SAFARI_MODE);
     FlagClear(FLAG_SYS_USE_STRENGTH);
     FlagClear(FLAG_SYS_USE_FLASH);
+    FlagClear(FLAG_SYS_NO_BAG_USE);
     FlagClear(FLAG_SYS_NO_CATCHING);
+    FlagClear(FLAG_SYS_NO_RUNNNING);
     // If you were defeated by Kyogre/Groudon and the step counter has
     // maxed out, end the abnormal weather.
     if (VarGet(VAR_SHOULD_END_ABNORMAL_WEATHER) == 1)
