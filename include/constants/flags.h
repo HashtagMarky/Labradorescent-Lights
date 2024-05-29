f99e073fc8a56cde6ce12396d021cccdd3a3e9a5
HashtagMarky <samuel.mark01@outlook.com>
2024-05-29 10:16:00 +0100

    Add Beginning Implementation for Aqua Intro in Borea Passage

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1320,7 +1320,8 @@
 #define FLAG_UNUSED_0x4EC                                           0x4EC // Unused Flag
 #define FLAG_UNUSED_0x4ED                                           0x4ED // Unused Flag
 #define FLAG_UNUSED_0x4EE                                           0x4EE // Unused Flag
-#define FLAG_UNUSED_0x4EF                                           0x4EF // Unused Flag
+
+#define FLAG_HIDE_AQUA_IN_BOREA_PASSAGE                             0x4EF // Unused Flag
 
 #define FLAG_DEFEATED_RUSTBORO_GYM                                  0x4F0
 #define FLAG_DEFEATED_DEWFORD_GYM                                   0x4F1
